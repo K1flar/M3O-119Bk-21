a11d930480e477c60243758d92c16162101bf446
_undefined <39531257+K1flar@users.noreply.github.com>
2021-09-15 00:13:59 +0300

    test2

---
+++ informatics/laboratory_work1/code.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+
+    float x;
+    float eps;
+    cout << "Введите x (от -1 до 1), Eps: ";
+    cin >> x >> eps;
+
+    // входной контроль
+    if (eps <= 0) {
+        cout << "\n Eps должно быть больше 0! \n";
+        return 0;
+    }
+    if (abs(x) >= 1) {
+        cout << "\n |x| должен быть меньше единицы! \n";
+        return 0;
+    }
+
+    // инициализация переменных
+    int n = 0,
+        znak = 1,
+        znam = 1;
+    float tsum = atan(x),
+        argx = x,
+        slag = x,
+        sum = x,
+        diff = abs(tsum - sum);
+
+    // рассчёт суммы ряда
+    while (diff > eps) {
+        n++;
+        znak = -znak;
+        argx = argx * x * x;
+        znam = znam + 2;
+        slag = znak * argx / znam;
+        sum = sum + slag;
+        diff = abs(tsum - sum);
+    }
+
+    cout << "n = " << n << " \n arctg(x)= " << tsum << " \n Сумма ряда: " << sum << " \n Разница: " << diff << " \n Погрешность (Eps): " << eps << "\n";
+    return 0;
+}
