be3f697685ce8028d6f2023f92e2171defc7babf
_undefined <39531257+K1flar@users.noreply.github.com>
2021-09-23 18:07:21 +0300

    fix

--- informatics/laboratory_work1/code.cpp
+++ informatics/laboratory_work1/code.cpp
@@ -1,33 +1,65 @@
-﻿#include <iostream>
+﻿/*-----------------------------------------------------------------------*
+|                                                                        |
+| Project type   : Win32 Console Application                             |
+| Project name   : laboratory_work1                                      |
+| File name      : code.cpp                                              |
+| Language       : C++, MS VS 2019                                       |
+| Programmer(1)  : Давыдов Денис Вадимови                                |
+| Programmer(2)  : Таласов Данил Сергеевич                               |
+| Programmer(3)  : Коцур Савелий Витальевич                              |
+| Created        : 15.07.21                                              |
+| Last revision  : 22.09.21                                              |
+| Comment        : ЛР сделана с помощью удаленного репозитория на GitHub |
+| Link to Github : https://github.com/K1flar/M3O-119Bk-21                |
+|                                                                        |
+*------------------------------------------------------------------------*
+| Кафедра 304                                                            |
+| Курс      : информатика                                                |
+| Задание 1 : Вычисление суммы бесконечного числового ряда               |
+*------------------------------------------------------------------------*
+|                            ВАРИАНТ №12                                 |
+*-----------------------------------------------------------------------*/
+
+#include <iostream>
 using namespace std;
 
 int main() {
     setlocale(LC_ALL, "Russian");
 
-    float x;
-    float eps;
-    cout << "Введите x (от -1 до 1), Eps: ";
+    // декларация переменных
+    float x;    // значение аргумента x
+    float eps;  // погрешность для вычисления arctg(x)
+    float argx; // числитель слагаемого элемента
+    float slag; // значение текущего элемента суммы ряда
+    float tsum; // точное значение arctg(x)
+    float sum;  // текущая сумма ряда
+    float diff; // разница между точной суммой и текущей суммой ряда
+    int n;      // текушее количество элементов суммы ряда
+    int znak;   // знак текущего слагаемого 
+    int znam;   // знаменатель слагаемого 
+
+    cout << "Введите x (от -1 до 1), затем Eps: ";
     cin >> x >> eps;
 
     // входной контроль
     if (eps <= 0) {
-        cout << "\n Eps должно быть больше 0! \n";
+        cout << "\nEps должно быть больше 0! \n";
         return 0;
     }
     if (abs(x) >= 1) {
-        cout << "\n |x| должен быть меньше единицы! \n";
+        cout << "\n|x| должен быть меньше единицы! \n";
         return 0;
     }
 
     // инициализация переменных
-    int n = 0,
-        znak = 1,
-        znam = 1;
-    float tsum = atan(x),
-        argx = x,
-        slag = x,
-        sum = x,
-        diff = abs(tsum - sum);
+    n = 0;
+    znak = 1;
+    znam = 1;
+    tsum = atan(x);
+    argx = x;
+    slag = x;
+    sum = x;
+    diff = abs(tsum - sum);
 
     // рассчёт суммы ряда
     while (diff > eps) {
@@ -40,6 +72,11 @@ int main() {
         diff = abs(tsum - sum);
     }
 
-    cout << "n = " << n << " \narctg(x)= " << tsum << " \nСумма ряда: " << sum << " \nРазница: " << diff << " \nПогрешность (Eps): " << eps << "\n";
+    // печать результата
+    cout << "n = " << n << "\n";
+    cout << "arctg(x) = " << tsum << "\n";
+    cout << "Сумма ряда: " << sum << "\n";
+    cout << "Разница: " << diff << "\n"; 
+    cout << "Погрешность(Eps): " << eps << "\n";
     return 0;
 }
